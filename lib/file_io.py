e688d4caf2b59855ad7df1311331b9fb4be31dd6
brevre <brendamakena09@gmail.com>
2025-03-06 07:44:29 +0300

    lab completed

--- lib/file_io.py
+++ lib/file_io.py
@@ -6,3 +6,31 @@ def append_file(file_name, append_content):
 
 def read_file(file_name):
     pass
+def write_file(file_name, file_content):
+    pass
+
+def append_file(file_name, append_content):
+    pass
+
+def read_file(file_name):
+    pass
+
+
+import os
+
+def write_file(file_name, file_content):
+    """Creates a new .txt file and writes the content to it."""
+    with open(f"{file_name}.txt", "w") as file:
+        file.write(file_content )
+
+def append_file(file_name, append_content):
+    """Appends content to an existing .txt file."""
+    with open(f"{file_name}.txt", "a") as file:
+        file.write(append_content )
+
+def read_file(file_name):
+    """Reads the content of a .txt file and returns it."""
+    with open(f"{file_name}.txt", "r") as file:
+        return file.read().strip()
+
+
